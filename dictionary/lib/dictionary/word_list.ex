357851d915d31519a37bdf33ae5acaa7cc8fd98f
Emilio <thecraftedgem@tuta.io>
2019-06-12 09:58:34 -0600

    Refactored Dictionary To Use Agents

--- dictionary/lib/dictionary/word_list.ex
+++ dictionary/lib/dictionary/word_list.ex
@@ -1,12 +1,16 @@
 defmodule Dictionary.WordList do
 
+  def start_link() do
+    Agent.start_link(&word_list/0)
+  end
+
   def word_list() do
     Path.expand("../../assets/word.txt", __DIR__)
     |> File.read!()
     |> String.split("\n")
   end
 
-  def random_word(words)  do
-    Enum.random(words)
+  def random_word(agent)  do
+    Agent.get(agent, &Enum.random/1)
   end
 end
